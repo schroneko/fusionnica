c454228aea62d586a451d402bfe9412749fc4038
schroneko <schroneko.fuller@gmail.com>
2021-08-08 14:38:54 +0900

    fix: design

---
+++ docs/what-is-the-fusion.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 2
+title: "核融合とは何か？"
+---
+
+aaa
---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Fusionnica",
+  tagline: "Fusionnica is the wiki about Nuclear Fusion Tech.",
+  url: "https://www.fusionnica.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "schroneko", // Usually your GitHub org/user name.
+  projectName: "fusionnica", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Fusionnica",
+      logo: {
+        alt: "Fusionnica Logo",
+        src: "img/logo.png",
+      },
+      items: [
+        {
+          href: "https://github.com/schroneko/fusionnica",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Determinant, Inc. All rights reserved.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+    colorMode: {
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        lightIcon: " ",
+        darkIcon: " ",
+      },
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/schroneko/fusionnica/edit/master/website/",
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        // },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
